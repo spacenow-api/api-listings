5e9b5bfa99fa34ed28abd4c03ba02fb043407439
Lucas <csalucasnascimento@gmail.com>
2019-10-11 17:12:45 +1100

    removing config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "spacenowtest" || process.env.DATABASE_USERNAME,
-    password: "Spac.918273!" || process.env.DATABASE_PASSWORD,
-    database: "spacenow_2019" || process.env.DATABASE_DATABASE,
-    host: "spacenow-test.cjo4zy3wnflc.ap-southeast-2.rds.amazonaws.com" || process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql",
     logging: true,
     underscored: true,
