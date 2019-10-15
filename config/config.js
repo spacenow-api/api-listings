34e231aac6bc92060ee6094dc11b2739e2a29302
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-10-15 19:52:03 +1100

    removing database vars

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME || 'spacenowtest',
-    password: process.env.DATABASE_PASSWORD || 'Spac.918273!',
-    database: process.env.DATABASE_SCHEMA || 'spacenow_2019',
-    host: process.env.DATABASE_HOST || 'spacenow-test.cjo4zy3wnflc.ap-southeast-2.rds.amazonaws.com',
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql",
     logging: true,
     underscored: true,
